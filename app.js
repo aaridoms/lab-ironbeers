0114860d1137f843d0799a058896d07a2de4c809
Ángel Arias <angadominguez96@gmail.com>
2023-08-02 17:33:17 +0200

    iteration 4 done

--- app.js
+++ app.js
@@ -18,43 +18,47 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Add the route handlers here:
 
-
 app.get('/', (req, res) => {
   res.render('index.hbs');
 });
 
 app.get('/beers', (req, res, next) => {
-   let beersArr =[]
-  punkAPI.getBeers()
- 
-  .then ((response) => {
-    for (let i = 0; i<25; i++) {
-    beersArr.push(response[i])
+  let beersArr = [];
+  punkAPI
+    .getBeers()
 
-    }
+    .then(response => {
+      for (let i = 0; i < 25; i++) {
+        beersArr.push(response[i]);
+      }
 
-     res.render('beers.hbs', {
-      beersArr: beersArr,
-     })
-    console.log(response)
-    console.log(beersArr)
-
-  })
-.catch((error) => {
-  console.log(error)
-})
-
- 
-})
+      res.render('beers.hbs', {
+        beersArr: beersArr
+      });
+      console.log(response);
+      console.log(beersArr);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
 
 app.get('/random-beer', (req, res, next) => {
-  res.render('random-beer.hbs')
-})
+
+  punkAPI.getRandom()
+  .then((response) => {
+    console.log(response)
+    res.render('random-beer.hbs', {
+      randomBeer: response
+    });
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+});
 
 app.get('*', (req, res, next) => {
-  res.send("error, pagina no encontrada")
-})
-
-
+  res.send('error, pagina no encontrada');
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
