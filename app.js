1c42806efef810cd0d97b90677ef42c45fb684f5
Ángel Arias <angadominguez96@gmail.com>
2023-08-02 16:48:09 +0200

    iteration 1 y 2 done

--- app.js
+++ app.js
@@ -18,8 +18,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Add the route handlers here:
 
+
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index.hbs');
 });
+
+app.get('/beers', (req, res, next) => {
+  res.render('beers.hbs')
+})
+
+app.get('/random-beer', (req, res, next) => {
+  res.render('random-beer.hbs')
+})
+
+app.get('*', (req, res, next) => {
+  res.send("error, pagina no encontrada")
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
